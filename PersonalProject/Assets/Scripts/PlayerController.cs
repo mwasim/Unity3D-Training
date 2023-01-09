b10debba18c76c6ef20d1a0a0d7e365a96dadfea
Wasim <mwasimse@gmail.com>
2023-01-09 19:36:51 +0500

    MyGame_BasicPrimitives - Code refactored

--- PersonalProject/Assets/Scripts/PlayerController.cs
+++ PersonalProject/Assets/Scripts/PlayerController.cs
@@ -1,17 +1,14 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float _speed = 10f;
 
-    private Rigidbody _rbPlayer;
     private float _verticalMovement, _horizontalMovement;
-
     private float _zBound = 3.5f;
 
-
+    private Rigidbody _rbPlayer;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -21,12 +18,22 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        MovePlayer();
+
+        ConstraintPlayerMovement();
+    }
+
+    private void MovePlayer()
+    {
         _horizontalMovement = Input.GetAxis("Horizontal");
         _verticalMovement = Input.GetAxis("Vertical");
 
         _rbPlayer.AddForce(Vector3.forward * _speed * _verticalMovement);
         _rbPlayer.AddForce(Vector3.right * _speed * _horizontalMovement);
+    }
 
+    private void ConstraintPlayerMovement()
+    {
         if (transform.position.z < -_zBound) transform.position = new Vector3(transform.position.x, transform.position.y, -_zBound);
         if (transform.position.z > _zBound) transform.position = new Vector3(transform.position.x, transform.position.y, _zBound);
     }
