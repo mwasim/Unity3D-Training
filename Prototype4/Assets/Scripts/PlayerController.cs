28640e18c25991c62ece88ea531c5f37bceaa9b1
Wasim <mwasimse@gmail.com>
2023-01-12 13:27:12 +0500

    Prototype4 - implemented features to rotate camera aroudn the focal point and moving the player in the direction of the focal point

---
+++ Prototype4/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float _speed;
+
+    private Rigidbody _playerRb;
+    private GameObject _focalPoint;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _playerRb = GetComponent<Rigidbody>();
+        _focalPoint = GameObject.Find("FocalPoint");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var forwardInput = Input.GetAxis("Vertical");
+
+        //forward direction of the focalPoint
+        _playerRb.AddForce(_focalPoint.transform.forward * _speed * forwardInput);
+    }
+}
