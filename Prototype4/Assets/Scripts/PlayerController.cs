0e20ad51209fcabc11c37d6ec4985c50e7271303
Wasim <mwasimse@gmail.com>
2023-01-13 14:45:13 +0500

    Prototype4 - Implemented Powerups: activating or deactivating powerups to bounce enemies farther

--- Prototype4/Assets/Scripts/PlayerController.cs
+++ Prototype4/Assets/Scripts/PlayerController.cs
@@ -5,9 +5,12 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float _speed;
-
+    [SerializeField] GameObject _powerupIndicator;
     private Rigidbody _playerRb;
     private GameObject _focalPoint;
+
+    private bool _hasPowerup;
+    private float _powerStength = 15.0f;
 
     // Start is called before the first frame update
     void Start()
@@ -23,5 +26,42 @@ public class PlayerController : MonoBehaviour
 
         //forward direction of the focalPoint
         _playerRb.AddForce(_focalPoint.transform.forward * _speed * forwardInput);
+
+        _powerupIndicator.transform.position = transform.position + new Vector3(0, -0.5f, 0);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Powerup"))
+        {
+            _hasPowerup = true;
+            _powerupIndicator.SetActive(true);            
+
+            Destroy(other.gameObject);
+
+            StartCoroutine(PowerupCountdownRoutine());
+        }
+    }
+
+    private IEnumerator PowerupCountdownRoutine()
+    {
+        yield return new WaitForSeconds(10);
+
+        _hasPowerup = false;
+        _powerupIndicator.SetActive(false);
+        //Debug.Log("POWER UP DEACTIVATED...");
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy") && _hasPowerup)
+        {
+            var enemyRb = collision.gameObject.GetComponent<Rigidbody>();
+            var awayFromPlayerDirection = collision.gameObject.transform.position - transform.position;
+
+            //Debug.Log($"Collided with: {collision.gameObject.name} with powerup set to : {_hasPowerup}");
+
+            enemyRb.AddForce(awayFromPlayerDirection * _powerStength, ForceMode.Impulse);
+        }
     }
 }
